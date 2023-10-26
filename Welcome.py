caee10756ccd01cf7e6f07e2b9ad7812d86cc03e
it21303098 <95157220+SenuriKaveesha@users.noreply.github.com>
2023-10-26 06:33:15 +0530

    change

--- Welcome.py
+++ Welcome.py
@@ -17,8 +17,18 @@ st_lottie(
             speed =1,
             reverse = False,
             loop = True
-        
         )
+
+def load_lottiefile(filepath: str):
+            with open(filepath, "r") as f:
+                return json.load(f)
+lottie_coding = load_lottiefile("")
+st_lottie(
+    lottie_coding,
+    speed = 1
+    reverse = False
+    loop = True
+)
    
 
 app()
