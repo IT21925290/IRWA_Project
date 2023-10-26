cdf76492addc004347947defa273db184fe16306
it21303098 <95157220+SenuriKaveesha@users.noreply.github.com>
2023-10-26 06:29:16 +0530

    animation added

--- Welcome.py
+++ Welcome.py
@@ -4,5 +4,20 @@ from streamlit_lottie import st_lottie
 
 def app():
     st.header('Welcome to the Movie Recommender System')
-    
+ 
+
+def load_lottiefile(filepath: str):
+            with open(filepath, "r") as f:
+                return json.load(f)
+
+        
+lottie_coding = load_lottiefile("./Animation/s2.json")
+st_lottie(
+            lottie_coding,
+            speed =1,
+            reverse = False,
+            loop = True
+        )
+   
+
 app()
